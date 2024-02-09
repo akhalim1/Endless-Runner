2144cb869b73c088aeae72e499c0c4a654563e7a
Alexander Halim <akhalim@ucsc.edu>
2024-02-09 08:50:53 -0800

    Restructuring code into scenes: moving contents of main.js to PlayScene

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@
 // Name: Alexander Halim
 // Date: 2/1/24
 
-// Spritesheet by ElvGames: https://elv-games.itch.io/free-fantasy-dreamland-sprites
-
 /* notes here
-IDEA: UNDERWATER ODYSSEY
+IDEA: (GAME NAME) UNDERWATER ODYSSEY
 
 Theme: Submarine avoiding underwater hazards like coral, trenches, scary aquatic creatures (sharks, kraken?).
 
@@ -16,10 +14,18 @@ Theme: Submarine avoiding underwater hazards like coral, trenches, scary aquatic
 
 "use strict";
 
+const WIDTH = 800;
+const HEIGHT = 600;
+const SUB_POSITION = { x: WIDTH * 0.05, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: SUB_POSITION,
+};
 let config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   render: {
     pixelArt: true,
   },
@@ -29,79 +35,8 @@ let config = {
       debug: true,
     },
   },
-  scene: {
-    preload,
-    create,
-    update,
-  },
+  scene: [new PlayScene(SHARED_CONFIG)],
 };
-
-function preload() {
-  this.load.image("ocean", "assets/ocean.png");
-  this.load.image("submarine", "assets/submarine.png");
-  this.load.image("shark", "assets/shark.png");
-}
-
-const VELOCITY = 200;
-let floatVelocity = 250;
-let submarine = null;
-let shark = null;
-const initialSubPos = { x: config.width * 0.05, y: config.height / 2 };
-
-function create() {
-  this.add.image(0, 0, "ocean").setOrigin(0, 0);
-
-  submarine = this.physics.add
-    .sprite(initialSubPos.x, initialSubPos.y, "submarine")
-    .setOrigin(0)
-    .setScale(0.1);
-
-  submarine.body.gravity.y = 400;
-
-  //submarine.body.velocity.x = VELOCITY;
-
-  //shark = this.physics.add.sprite(300, 100, "shark").setOrigin(0, 0);
-  this.sharks = this.physics.add.group({
-    key: "shark",
-    repeat: 2,
-    setXY: { x: 900, y: 100, stepX: -300, stepY: 20 },
-  });
-
-  this.sharks.children.iterate((shark) => {
-    shark.setVelocityX(-VELOCITY);
-  });
-
-  this.input.on("pointerdown", float);
-
-  let spaceBar = this.input.keyboard.addKey(
-    Phaser.Input.Keyboard.KeyCodes.SPACE
-  );
-
-  spaceBar.on("down", float);
-}
-
-function update(time, delta) {
-  if (submarine.y > config.height || submarine.y < -submarine.height) {
-    restartSubPosition();
-  }
-
-  //shark movement here
-  this.sharks.children.iterate((shark) => {
-    if (shark.x < -shark.width) {
-      shark.x = config.width + Phaser.Math.Between(100, 300);
-      shark.y = Phaser.Math.Between(0, config.height - shark.height);
-    }
-  });
-}
-
-function restartSubPosition() {
-  submarine.x = initialSubPos.x;
-  submarine.y = initialSubPos.y;
-  submarine.body.velocity.y = 0;
-}
-function float() {
-  submarine.body.velocity.y = -floatVelocity;
-}
 
 let game = new Phaser.Game(config);
 
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,13 +1,86 @@
 class PlayScene extends Phaser.Scene {
   constructor(config) {
     super("PlayScene");
+    /*
+    this.initialSubPos = {
+      x: 100,
+      y: 400,
+    };
+    */
+    this.config = config;
+    this.submarine = null;
+    this.sharks = null;
+    this.floatVelocity = 250;
+  }
+
+  preload() {
+    this.load.image("ocean", "assets/ocean.png");
+    this.load.image("submarine", "assets/submarine.png");
+    this.load.image("shark", "assets/shark.png");
   }
 
   create() {
     this.sky = this.add.tileSprite(0, 0, 640, 480, "sky").setOrigin(0, 0);
+    this.add.image(0, 0, "ocean").setOrigin(0, 0);
+
+    this.submarine = this.physics.add
+      .sprite(
+        this.config.startPosition.x,
+        this.config.startPosition.y,
+        "submarine"
+      )
+      .setOrigin(0)
+      .setScale(0.1);
+
+    this.submarine.body.gravity.y = 400;
+
+    //shark = this.physics.add.sprite(300, 100, "shark").setOrigin(0, 0);
+    this.sharks = this.physics.add.group({
+      key: "shark",
+      repeat: 2,
+      setXY: { x: 900, y: 100, stepX: -300, stepY: 20 },
+    });
+
+    this.sharks.children.iterate((shark) => {
+      shark.setVelocityX(-200);
+    });
+
+    // purpose of passing "this" (3rd argument): you need to provide the value of the context you want to pass into the function float. By passing "this", you will get the correct context and submarine will be defined.
+    this.input.on("pointerdown", this.float, this);
+
+    let spaceBar = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+
+    spaceBar.on("down", this.float, this);
   }
 
   update() {
     this.sky.tilePositionX -= 1;
+
+    if (
+      this.submarine.y > config.height ||
+      this.submarine.y < -this.submarine.height
+    ) {
+      this.restartSubPosition();
+    }
+
+    //shark movement here
+    this.sharks.children.iterate((shark) => {
+      if (shark.x < -shark.width) {
+        shark.x = config.width + Phaser.Math.Between(100, 300);
+        shark.y = Phaser.Math.Between(0, config.height - shark.height);
+      }
+    });
+  }
+
+  restartSubPosition() {
+    this.submarine.x = this.config.startPosition.x;
+    this.submarine.y = this.config.startPosition.y;
+    this.submarine.body.velocity.y = 0;
+  }
+
+  float() {
+    this.submarine.body.velocity.y = -this.floatVelocity;
   }
 }
